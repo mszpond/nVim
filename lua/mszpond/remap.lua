4ab168bd348aad4a0c81fe9241d05376620ee600
Michał Szpond <mszpond@opteam.pl>
2023-04-11 21:09:36 +0200

    fuzzyFinder & colorscheme

--- lua/mszpond/remap.lua
+++ lua/mszpond/remap.lua
@@ -1,5 +1,2 @@
 vim.g.mapleader = " "
-
-GGGGet-Winevent -LogName security -MaxEvents 5000|? {$_.id -eq 4776 -and $_.message -like "*kdutka*06a*"}| fl:
-et-Winevent -LogName security -MaxEvents 5000|? {$_.id -eq 4776 -and $_.message -like "*kdutka*06a*"}| fl:
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
